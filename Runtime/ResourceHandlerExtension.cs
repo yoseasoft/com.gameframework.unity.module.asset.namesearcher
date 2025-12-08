caee800128eebe37fbb5ea05a93fc667ce02d4bd
hurley <yoseasoft@163.com>
2025-12-08 20:21:50 +0800

    定义接口规范；

--- Runtime/ResourceHandlerExtension.cs
+++ Runtime/ResourceHandlerExtension.cs
@@ -7,6 +7,7 @@
 /// </summary>
 
 using System;
+using Cysharp.Threading.Tasks;
 
 using UnityObject = UnityEngine.Object;
 
@@ -20,12 +21,33 @@ namespace Game.Module.Asset.NameSearcher
         /// <summary>
         /// 同步加载资源
         /// </summary>
-        /// <param name="name">资源名字</param>
+        /// <param name="name">资源名字k</param>
         /// <param name="type">资源类型</param>
         public static UnityObject LoadAssetByName(this GameEngine.ResourceHandler self, string name, Type type)
         {
             string url = ConvertAssetNameToUrl(name);
             return self.LoadAsset(url, type);
+        }
+
+        /// <summary>
+        /// 异步加载资源
+        /// </summary>
+        /// <param name="name">资源名字</param>
+        /// <param name="completed">加载完成回调</param>
+        public static GooAsset.Asset LoadAssetAsync<T>(this GameEngine.ResourceHandler self, string name, Action<UnityObject> completed) where T : UnityObject
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return self.LoadAssetAsync<T>(url, completed);
+        }
+
+        /// <summary>
+        /// 异步加载资源
+        /// </summary>
+        /// <param name="name">资源名字</param>
+        public static async UniTask<T> LoadAssetAsyncByName<T>(this GameEngine.ResourceHandler self, string name) where T : UnityObject
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return await self.LoadAssetAsync<T>(url);
         }
 
         static string ConvertAssetNameToUrl(string name)
