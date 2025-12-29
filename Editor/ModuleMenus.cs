654a13138d72176438d7ffb91c23df076c909757
hurley <yoseasoft@163.com>
2025-12-29 14:53:19 +0800

    增加名称索引文件的导出及资源访问接口函数；

--- Editor/ModuleMenus.cs
+++ Editor/ModuleMenus.cs
@@ -6,22 +6,184 @@
 /// 功能描述：
 /// </summary>
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 using UnityEditor;
 using UnityEngine;
 
+using GooAsset;
+using GooAsset.Editor.Build;
+
 namespace Game.Module.Asset.NameSearcher.Editor
 {
+    [Serializable]
+    internal class ManifestRoot
+    {
+        public List<ManifestBundleInfo> manifestBundleInfoList; // 资源包信息数组
+    }
+
     /// <summary>
     /// 模块菜单
     /// </summary>
     static class ModuleMenus
     {
         const string MenuName_BuildAssetNameSearchTable = @"GooAsset/Module/构建资源名称索引文件";
+        const string MenuName_CopyAssetNameSearchTableToStreamingAssetPath = @"GooAsset/Module/复制资源名称索引文件到StreamingAsset目录";
 
-        [MenuItem(MenuName_BuildAssetNameSearchTable)]
+        #region 生成资源名称索引文件
+
+        [MenuItem(MenuName_BuildAssetNameSearchTable, priority = 1000)]
         static void OnBuildAssetNameSearchTable()
         {
-            Debug.Log("开始构建资源名称索引文件");
+            GetAllAssetJson(BuildUtils.PlatformBuildPath);
         }
+
+        [MenuItem(MenuName_CopyAssetNameSearchTableToStreamingAssetPath, priority = 1001)]
+        static void OnCopyAssetMappingToStreamingAssetPath()
+        {
+            CopyFileToStreamingAssetsPath(ResourceHandlerExtension.AssetNameSearcherFile);
+        }
+
+        /// <summary>
+        /// 将json配置生成映射表
+        /// </summary>
+        /// <param name="path"></param>
+        static void GetAllAssetJson(string path)
+        {
+            //1. 遍历所有资源配置，获取配置名
+            string configNames = string.Empty;
+            List<string> namelist = new List<string>();
+            foreach (ManifestConfig manifestConfig in BuildUtils.GetAllManifestConfigs())
+            {
+                namelist.Add(manifestConfig.name);
+            }
+
+            Dictionary<string, string> pathsDict = new Dictionary<string, string>();
+            // 2. 获取 JSON 文件所在目录
+            string jsonFolderPath = path;
+
+            // 3. 遍历目录下所有 .json 文件
+            string[] jsonFiles = Directory.GetFiles(jsonFolderPath, "*.json", SearchOption.AllDirectories);
+
+            // 4. 逐个处理 JSON 文件
+            foreach (string filePath in jsonFiles)
+            {
+                bool isHad = false;
+                foreach (string configName in namelist)
+                {
+                    if (filePath.Contains(configName))
+                    {
+                        isHad = true;
+                        break;
+                    }
+                }
+                if (!isHad)
+                {
+                    Debug.Log($"--无--处理文件：{filePath}");
+                    continue;
+
+                }
+                Debug.Log($"---处理文件：{filePath}");
+                try
+                {
+                    string jsonContent = File.ReadAllText(filePath); // 读取文件内容
+                    ManifestRoot root = JsonUtility.FromJson<ManifestRoot>(jsonContent); // 反序列化
+
+                    if (root != null && root.manifestBundleInfoList != null)
+                    {
+                        foreach (var bundle in root.manifestBundleInfoList)
+                        {
+                            foreach (string assetPath in bundle.a)
+                            {
+                                string assetName = Path.GetFileName(assetPath);
+                                // Debug.Log(resourceName + "-----File------" + resourcePath);
+                                // 将键值对写入目标字典
+                                if (!pathsDict.ContainsKey(assetName))
+                                {
+                                    pathsDict[assetName] = assetPath;
+                                }
+                                else
+                                {
+                                    Debug.LogError("不允许文件名重复，文件名：" + assetName);
+                                }
+                            }
+
+                            //Debug.Log($"\n资源名：{bundle.n}");
+                            //Debug.Log($"是否原始文件：{bundle.IsRawFile}");
+                            //Debug.Log($"资源路径：{string.Join(", ", bundle.a)}");
+                            //Debug.Log($"资源大小：{bundle.s} 字节");
+                            //Debug.Log($"资源哈希：{bundle.h}");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError($"反序列化失败，文件结构不匹配：{filePath}");
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError($"处理文件 {filePath} 时出错：{e.Message}");
+                }
+            }
+
+            SaveDictToJson(pathsDict, ResourceHandlerExtension.AssetNameSearcherFile);
+        }
+
+        /// <summary>
+        /// 将 Dictionary<string, string> 保存为 JSON 文件
+        /// </summary>
+        /// <param name="dict">要保存的字典</param>
+        /// <param name="fileName">文件名</param>
+        public static void SaveDictToJson(Dictionary<string, string> dict, string fileName)
+        {
+
+            // 1. 序列化字典为 JSON 字符串
+            string json = LitJson.JsonMapper.ToJson(dict);
+
+            // 2. 确定文件保存路径
+            string savePath = Path.Combine(BuildUtils.PlatformBuildPath, fileName);
+
+            try
+            {
+                // 3. 写入 JSON 字符串到文件
+                File.WriteAllText(savePath, json);
+                Debug.Log($"JSON 文件保存成功！路径：{savePath}");
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"JSON 文件保存失败：{e.Message}");
+            }
+        }
+
+        /// <summary>
+        /// 拷贝fromFileName文件到StreamingAssetsPath目录下
+        /// </summary>
+        /// <param name="fromFileName"></param>
+        /// <param name="destFileName"></param>
+        static void CopyFileToStreamingAssetsPath(string fromFileName, string destFileName = null)
+        {
+            // 不传入目标文件名时使用源文件名
+            destFileName ??= fromFileName;
+
+            string srcFilePath = BuildUtils.TranslateToBuildPath(fromFileName);
+            string destFilePath = AssetPath.CombinePath(BuildUtils.BuildLocalDataPath, destFileName);
+            if (!File.Exists(srcFilePath))
+            {
+                Debug.LogWarning($"所需首包文件{srcFilePath}不存在, 请检查原因(例:是否打成游戏安装包前没有构建资源包？)");
+                return;
+            }
+
+            string destFolderPath = Path.GetDirectoryName(destFilePath);
+            if (!Directory.Exists(destFolderPath))
+            {
+                Directory.CreateDirectory(destFolderPath);
+            }
+
+            File.Copy(srcFilePath, destFilePath, true);
+        }
+
+        #endregion
     }
 }
--- Runtime/ResourceHandlerExtension.cs
+++ Runtime/ResourceHandlerExtension.cs
@@ -7,6 +7,9 @@
 /// </summary>
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using Cysharp.Threading.Tasks;
 
 using UnityObject = UnityEngine.Object;
@@ -18,6 +21,13 @@ namespace Game.Module.Asset.NameSearcher
     /// </summary>
     public static class ResourceHandlerExtension
     {
+        public const string AssetNameSearcherFile = @"AssetNameSearchList.json";
+
+        /// <summary>
+        /// 记录资源文件名和路径的映射关系的管理容器
+        /// </summary>
+        private static IDictionary<string, string> _assetPathMappingDict = new Dictionary<string, string>();
+
         /// <summary>
         /// 同步加载资源
         /// </summary>
@@ -34,7 +44,7 @@ namespace Game.Module.Asset.NameSearcher
         /// </summary>
         /// <param name="name">资源名字</param>
         /// <param name="completed">加载完成回调</param>
-        public static GooAsset.Asset LoadAssetAsync<T>(this GameEngine.ResourceHandler self, string name, Action<UnityObject> completed) where T : UnityObject
+        public static GooAsset.Asset LoadAssetAsyncByName<T>(this GameEngine.ResourceHandler self, string name, Action<UnityObject> completed) where T : UnityObject
         {
             string url = ConvertAssetNameToUrl(name);
             return self.LoadAssetAsync<T>(url, completed);
@@ -50,9 +60,132 @@ namespace Game.Module.Asset.NameSearcher
             return await self.LoadAssetAsync<T>(url);
         }
 
+        /// <summary>
+        /// 通过名称和类型异步加载资源
+        /// </summary>
+        /// <param name="name">资源名字</param>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        public static async UniTask<UnityObject> LoadAssetAsyncByName(this GameEngine.ResourceHandler self, string name, Type type)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return await self.LoadAssetAsync(url, type);
+        }
+
+        /// <summary>
+        /// 同步加载场景
+        /// </summary>
+        /// <param name="name">资源名字</param>
+        /// <param name="isAdditive">是否使用叠加方式加载</param>
+        public static GooAsset.Scene LoadSceneByName(this GameEngine.ResourceHandler self, string name, bool isAdditive = false)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return self.LoadScene(url, isAdditive);
+        }
+
+        /// <summary>
+        /// 异步加载场景(回调)
+        /// </summary>
+        /// <param name="name">资源地址(名字或路径)</param>
+        /// <param name="isAdditive">是否使用叠加方式加载</param>
+        /// <param name="completed">加载完成回调</param>
+        public static GooAsset.Scene LoadSceneAsyncByName(this GameEngine.ResourceHandler self, string name, bool isAdditive, System.Action<GooAsset.Scene> completed)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return self.LoadSceneAsync(url, isAdditive, completed);
+        }
+
+        /// <summary>
+        /// 异步加载场景
+        /// </summary>
+        /// <param name="url">资源地址(名字或路径)</param>
+        /// <param name="isAdditive">是否使用叠加方式加载</param>
+        public static async UniTask<GooAsset.Scene> LoadSceneAsyncByName(this GameEngine.ResourceHandler self, string name, bool isAdditive = false)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return await self.LoadSceneAsync(url, isAdditive);
+        }
+
+        /// <summary>
+        /// 同步加载原始流式文件(直接读取persistentDataPath中的文件, 然后可根据文件保存路径(RawFile.savePath)读取文件, 使用同步加载前需已保证文件更新)
+        /// <param name="name">文件原打包路径('%ORIGINAL_RESOURCE_PATH%/......', 若为Assets外部文件则为:'Assets文件夹同级目录/...'或'Assets文件夹同级文件')</param>
+        /// </summary>
+        public static GooAsset.RawFile LoadRawFileByName(this GameEngine.ResourceHandler self, string name)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return self.LoadRawFile(url);
+        }
+
+        /// <summary>
+        /// 异步加载原始流式文件(将所需的文件下载到persistentDataPath中, 完成后可根据文件保存路径(RawFile.savePath)读取文件)
+        /// /// <param name="name">文件原打包路径('%ORIGINAL_RESOURCE_PATH%/......', 若为Assets外部文件则为:'Assets文件夹同级目录/...'或'Assets文件夹同级文件')</param>
+        /// </summary>
+        public static GooAsset.RawFile LoadRawFileAsyncByName(this GameEngine.ResourceHandler self, string name, System.Action<GooAsset.RawFile> completed)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return self.LoadRawFileAsync(url, completed);
+        }
+
+        /// <summary>
+        /// 异步加载原始流式文件(将所需的文件下载到persistentDataPath中, 完成后可根据文件保存路径(RawFile.savePath)读取文件)
+        /// /// <param name="name">文件原打包路径('%ORIGINAL_RESOURCE_PATH%/......', 若为Assets外部文件则为:'Assets文件夹同级目录/...'或'Assets文件夹同级文件')</param>
+        /// </summary>
+        public static async UniTask<GooAsset.RawFile> LoadRawFileAsyncByName(this GameEngine.ResourceHandler self, string name)
+        {
+            string url = ConvertAssetNameToUrl(name);
+            return await self.LoadRawFileAsync(url);
+        }
+
+        /// <summary>
+        /// 使用前先预加载映射配置InitLoadAssetPathMapping
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         static string ConvertAssetNameToUrl(string name)
         {
-            return name;
+            return GetAssetUrlByFileName(name);
         }
+
+        #region 读取映射文件
+
+        /// <summary>
+        /// 加载资源映射json文件
+        /// </summary>
+        public static async UniTask InitLoadAssetPathMapping(this GameEngine.ResourceHandler self)
+        {
+            if (_assetPathMappingDict.Count == 0)
+            {
+                string jsonPath = GooAsset.AssetPath.TranslateToLocalDataPath(AssetNameSearcherFile);
+                string jsonContent = await ReadFileTextAsync(jsonPath);
+                _assetPathMappingDict = LitJson.JsonMapper.ToObject<Dictionary<string, string>>(jsonContent);
+            }
+        }
+
+        /// <summary>
+        /// 通过文件名获取文件路径
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        static string GetAssetUrlByFileName(string fileName)
+        {
+            if (_assetPathMappingDict.TryGetValue(fileName, out string fullPath))
+            {
+                return fullPath;
+            }
+            return string.Empty;
+        }
+
+        static async UniTask<string> ReadFileTextAsync(string filePath)
+        {
+            string text = await File.ReadAllTextAsync(filePath);
+            if (GooAsset.Configure.Secret.ManifestFileEncryptEnabled)
+            {
+                return GooAsset.Utility.Cryptography.Decrypt(text);
+            }
+
+            return text;
+        }
+
+        #endregion
     }
 }
